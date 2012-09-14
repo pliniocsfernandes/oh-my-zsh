650d1626f3cdf8c94b1a1f41355875f42dfe87f8
Plínio César Simões Fernandes <pliniocsfernandes@gmail.com>
2012-09-14 14:56:05 -0300

    Bugfix for phing plugin on Ubuntu 12.04

--- plugins/phing/phing.plugin.zsh
+++ plugins/phing/phing.plugin.zsh
@@ -1,8 +1,9 @@
+#!/bin/zsh
 _phing_does_target_list_need_generating () {
   if [ ! -f .phing_targets ]; then return 0;
   else
-    accurate=$(stat -f%m .phing_targets)
-    changed=$(stat -f%m build.xml)
+    accurate=$(stat --printf=%m .phing_targets)
+    changed=$(stat --printf=%m build.xml)
     return $(expr $accurate '>=' $changed)
   fi
 }
@@ -10,7 +11,7 @@ _phing_does_target_list_need_generating () {
 _phing () {
   if [ -f build.xml ]; then
     if _phing_does_target_list_need_generating; then
-      phing -l |grep -v ":" |grep -v "^$"|grep -v "\-" > .phing_targets
+      phing -l | grep -v ":" | grep -v "\-\-" | grep -v "^$" | awk 'FNR>2 {print $1}' > .phing_targets
     fi
     compadd `cat .phing_targets`
   fi
